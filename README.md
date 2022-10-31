29ba161cdf247beb869e726eadfffa92da14da7a
John Heaven <john@johnheaven.eu>
2022-10-31 12:08:22 +0100

    updated README.md, added loop over multiple sensors, moved ip_search into module

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # ambient-data-logger
-Pulls in data from an API (e.g. [Raspberry Pico with ambient data sensors](https://github.com/johnheaven/pico-ambient-data-api/tree/main)) and adds them to a CSV.
+Pulls in data from an API (e.g. [Raspberry Pico with ambient data sensors](https://github.com/johnheaven/pico-ambient-data-api/tree/main)) and adds them to a CSV, one for each sensor.
 
 It's designed to be run with a cron job, so doesn't do any timing.
 
-My current set-up is a Raspberry Pico that exposes a simple API on the local network. This has information about ambient data (temperature, pressure, humidity). It also contains an ID.
+My current set-up is a Raspberry Pico that exposes a simple API on the local network. This has information about ambient data (temperature, pressure, humidity). It also contains an ID and a UUID.
 
-The problem is that the Pico can't set a hostname, and my router doesn't allow me to allocate a fixed IP without turning off DHCP entirely. So the logger starts with a particular IP and then searches until it finds the right IP (i.e. it can access the JSON data and it checks that the ID is the one it's looking for). Then it logs the data to CSV.
+## Pico discovery hack
+
+The problem is that the Pico can't set a hostname, and my router doesn't allow me to allocate a fixed IP without turning off DHCP entirely. So the logger starts with a particular IP and then searches until it finds the right IP (i.e. it can access the JSON data and it checks that the ID is the one it's looking for). Then it logs the data to CSV. It caches the best IP to a file, so it can look that up next time.
+
+## Multiple Picos
+
+You might have several Picos collecting data in different rooms or places. Currently, it just saves a separate CSV for each of these as a good-enough solution.
+
+## Setup
+
+You need to copy the `example-settings.py` file to `settings.py` and enter the Pico IDs you want to search for (not UUIDs, although that might be a better approach in the future).
+
+## Designed for the Raspberry Pi
+
+I run this on my Raspberry Pi 4, which has an ancient Python 3 version installed on it (sigh). Thus, f-strings aren't supported, which is why I use the % notation.
--- ambient-data-logger.py
+++ ambient-data-logger.py
@@ -2,67 +2,71 @@ import requests
 import time
 import csv
 import json
-from os import environ
 
-from ip_search import ip_search
+from ip_search.ip_search import ip_search
+from settings import sensor_ids
 
-sensor_id = '1'
+all_ambient_data = []
 
+for sensor_id in sensor_ids:
 # last 3 digits of IP address so we can find the right one by trial and error
-try:
-    previous_best = json.load(open('best-ip-cache.json', 'r'))
-except FileNotFoundError as e:
-    print(e)
-    starting_ip_last_3 = 145
-else:
-    starting_ip_last_3 = previous_best
-
-ips = ip_search(starting_ip_last_3=starting_ip_last_3, max_steps=50)
-
-### GET DATA
-
-success = False
-
-# run a loop until we get success or we run out of IPs to try
-while (not success) and (ips.get_uri()):
     try:
-        pico_url = ips.get_uri()
-        print('Trying URL %s' % pico_url)
-        response = requests.get(url=pico_url, timeout=5)
-    except:
-        print('Connection attempt failed.')
+        previous_best = json.load(open('best-ip-cache__%s.json' % sensor_id, 'r'))
+    except FileNotFoundError as e:
+        print(e)
+        starting_ip_last_3 = 145
     else:
-        # get the response code
-        http_response = response.status_code
-        print ('HTTP response: ', http_response)
-        # 200 means success
-        if http_response == 200:
-            # get data from JSON
-            try:
-                ambient_data = response.json()
-            except ValueError as e:
-                print(e)
-            else:
-                # check whether it's from the right Pico (for the case that there are several)
-                if ambient_data['pico_id'] == sensor_id:
-                    success = True
-    # bump the IP up one so we can try it next.
-    # don't do this if success==True so we can preserve the right value for storing in env var later
-    if not success:
-        ips.bump_sensor_ip()
-        if not ips.get_uri():
-            print('Couldn\'t find device with sensor ID %s' % sensor_id)
+        starting_ip_last_3 = previous_best
+
+    ips = ip_search(starting_ip_last_3=starting_ip_last_3, max_steps=50)
+
+    ### GET DATA
+
+    success = False
+
+    # run a loop until we get success or we run out of IPs to try
+    while (not success) and (ips.get_uri()):
+        try:
+            pico_url = ips.get_uri()
+            print('Trying URL %s' % pico_url)
+            response = requests.get(url=pico_url, timeout=5)
+        except:
+            print('Connection attempt failed.')
+        else:
+            # get the response code
+            http_response = response.status_code
+            print ('HTTP response: ', http_response)
+            # 200 means success
+            if http_response == 200:
+                # get data from JSON
+                try:
+                    ambient_data = response.json()
+                except ValueError as e:
+                    print(e)
+                else:
+                    # check whether it's from the right Pico (for the case that there are several)
+                    if ambient_data['pico_id'] == sensor_id:
+                        success = True
+        # bump the IP up one so we can try it next.
+        # don't do this if success==True so we can preserve the right value for storing in env var later
+        if not success:
+            ips.bump_sensor_ip()
+            if not ips.get_uri():
+                print('Couldn\'t find device with sensor ID %s' % sensor_id)
 
 
-ambient_data['time'] = time.asctime(time.localtime())
-print(ambient_data)
+    ambient_data['time'] = time.asctime(time.localtime())
+    print(ambient_data)
 
-### WRITE IP TO JSON FOR NEXT TIME
-json.dump(ips.get_current(), open('best-ip-cache.json', 'w'))
+    ### WRITE IP TO JSON FOR NEXT TIME
+    json.dump(ips.get_current(), open('best-ip-cache__%s.json' % sensor_id, 'w'))
 
-### WRITE TO CSV
+    all_ambient_data.append(ambient_data)
+
+    ### WRITE TO CSV
 
 # assuming headers are already present
 with open('%s_ambient_data.csv' % sensor_id, mode='a', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=('time', 'temp', 'pressure', 'humidity', 'sensor', 'pico_id'))
-    writer.writerow(ambient_data)
+    writer = csv.DictWriter(csvfile, fieldnames=('time', 'temp', 'pressure', 'humidity', 'sensor', 'pico_id', 'pico_uuid'))
+    for ambient_data in all_ambient_data:
+        writer.writerow(ambient_data)
