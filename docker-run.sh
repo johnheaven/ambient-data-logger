6eb3ddb4a95dc8d23c727c865faaff0097926689
John Heaven <john@johnheaven.eu>
2022-11-12 13:38:58 +0100

    fix: docker-run.sh (so it runs non-interactively)

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 AMB_DAT_DATA_DIR=$1
 AMB_DAT_CACHE_DIR=$2
 
-docker run -it --rm --name ambient-data-logger --mount type=bind,src=${AMB_DAT_DATA_DIR},dst=/usr/src/data --mount type=bind,src=${AMB_DAT_CACHE_DIR},dst=/usr/src/cache ambient-data-logger
+docker run -d --rm --name ambient-data-logger --mount type=bind,src=${AMB_DAT_DATA_DIR},dst=/usr/src/data --mount type=bind,src=${AMB_DAT_CACHE_DIR},dst=/usr/src/cache ambient-data-logger
